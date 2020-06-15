c65d68bd8c226480af980d41fa85de9a35384fe5
skiyedev <65785285+skiyedev@users.noreply.github.com>
2020-06-15 18:12:54 +0300

    Delete Key2.cpp

--- keyboardcrash/Key2.cpp
+++
@@ -1,9 +0,0 @@
-#include "Key2.h"
-#include <QKeyEvent>
-#include <QGraphicsScene>
-#include "Note2.h"
-#include <stdlib.h>
-void Key2::keyPressEvent(QKeyEvent *event)
-{
-    if (event->key() == Qt::Key_W){}
-}
