c2969ac959037849deb63a0bb4d5ffe1c5298bf5
skiyedev <65785285+skiyedev@users.noreply.github.com>
2020-06-15 20:28:55 +0300

    Update Score.cpp

--- keyboardcrash/Score.cpp
+++ keyboardcrash/Score.cpp
@@ -1,21 +1,23 @@
 #include "Score.h"
 #include <QFont>
 
-Score::Score(QGraphicsItem *parent): QGraphicsTextItem(parent)
-{
+Score::Score(QGraphicsItem *parent): QGraphicsTextItem(parent) {
     score = 0;
     setPlainText(QString("Score: ") + QString::number(score));
     setDefaultTextColor(Qt::white);
     setFont(QFont("times",16));
 }
 
-void Score::increase()
-{
+void Score::increase() {
     score++;
     setPlainText(QString("Score: ") + QString::number(score));
 }
 
-int Score::getScore()
-{
+void Score::decrease() {
+    score -= 20;
+    setPlainText(QString("Score: ") + QString::number(score));
+}
+
+int Score::getScore() {
     return score;
 }
