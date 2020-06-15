1541761ae0970d914f9c086be52caf8e3a8645e9
skiyedev <65785285+skiyedev@users.noreply.github.com>
2020-06-15 20:10:37 +0300

    Update Key.cpp

--- keyboardcrash/Key.cpp
+++ keyboardcrash/Key.cpp
@@ -1,39 +1,50 @@
+#include <QKeyEvent>
 #include <QGraphicsScene>
+#include <stdlib.h>
 #include <QGraphicsItem>
+#include <QDebug>
 #include "Note1.h"
 #include "Note2.h"
 #include "Note3.h"
 #include "Note4.h"
-#include "SpawnArrow.h"
+#include "Score.h"
+#include "Key.h"
 
-void SpawnArrow::spawnArrow()
-{
-    srand(time(NULL));
-    int a = rand() % 8;
-    if (a < 5) {
-        Note1 * note1 = new Note1();
-        note1->setPos(x(), y()-850);
-        scene()->addItem(note1);
+extern Score * score;
+extern Note1 * note1;
+extern Note2 * note2;
+extern Note3 * note3;
+extern Note4 * note4;
+extern Key * key;
 
+void Key::keyPressEvent(QKeyEvent *event) {
+    if (event->key() == Qt::Key_Q) {
+        if (note1->pos().y() > key->pos().y() - 100 && note1->pos().y() < key->pos().y() + 100) {
+            score->increase();
+        } else {
+            score->decrease();
+        }
     }
-
-    a = rand() % 8;
-    if (a < 5) {
-        Note2 * note2 = new Note2();
-        note2->setPos(x()+100, y()-850);
-        scene()->addItem(note2);
+    if (event->key() == Qt::Key_W) {
+        if (note2->pos().y() > key->pos().y() - 100 && note1->pos().y() < key->pos().y() + 100) {
+            score->increase();
+        } else {
+            score->decrease();
+        }
     }
-
-    a = rand() % 8;
-    if (a < 5) {
-        Note3 * note3 = new Note3();
-        note3->setPos(x()+200, y()-850);
-        scene()->addItem(note3);
+    if (event->key() == Qt::Key_O) {
+        if (note3->pos().y() > key->pos().y() - 100 && note1->pos().y() < key->pos().y() + 100) {
+            score->increase();
+        } else {
+            score->decrease();
+        }
     }
-    a = rand() % 8;
-    if (a < 5) {
-        Note4 * note4 = new Note4();
-        note4->setPos(x()+300, y()-850);
-        scene()->addItem(note4);
+    if (event->key() == Qt::Key_P) {
+        if (note4->pos().y() > key->pos().y() - 100 && note1->pos().y() < key->pos().y() + 100) {
+            score->increase();
+        } else {
+            score->decrease();
+        }
     }
 }
+
