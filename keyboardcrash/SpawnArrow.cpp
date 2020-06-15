e40838a9c497fd9740ed9ae86267c1fa22debfb5
skiyedev <65785285+skiyedev@users.noreply.github.com>
2020-06-15 20:07:59 +0300

    Update SpawnArrow.cpp

--- keyboardcrash/SpawnArrow.cpp
+++ keyboardcrash/SpawnArrow.cpp
@@ -1,43 +1,39 @@
-#include "SpawnArrow.h"
-#include <QKeyEvent>
 #include <QGraphicsScene>
+#include <QGraphicsItem>
 #include "Note1.h"
 #include "Note2.h"
 #include "Note3.h"
 #include "Note4.h"
-#include "Score.h"
-#include <stdlib.h>
-#include <QGraphicsItem>
-#include <QDebug>
-
+#include "SpawnArrow.h"
 
 void SpawnArrow::spawnArrow()
 {
-    int a = rand()%8;
-    if(a<5){
+    srand(time(NULL));
+    int a = rand() % 8;
+    if (a < 5) {
         Note1 * note1 = new Note1();
-        note1->setPos(x(),y()-850);
+        note1->setPos(x(), y()-850);
         scene()->addItem(note1);
 
     }
 
-    a = rand()%8;
-    if(a<5){
+    a = rand() % 8;
+    if (a < 5) {
         Note2 * note2 = new Note2();
-        note2->setPos(x()+100,y()-850);
+        note2->setPos(x()+100, y()-850);
         scene()->addItem(note2);
     }
 
-    a = rand()%8;
-    if(a<5){
+    a = rand() % 8;
+    if (a < 5) {
         Note3 * note3 = new Note3();
-        note3->setPos(x()+200,y()-850);
+        note3->setPos(x()+200, y()-850);
         scene()->addItem(note3);
     }
-    a = rand()%8;
-    if(a<5){
+    a = rand() % 8;
+    if (a < 5) {
         Note4 * note4 = new Note4();
-        note4->setPos(x()+300,y()-850);
+        note4->setPos(x()+300, y()-850);
         scene()->addItem(note4);
     }
 }
