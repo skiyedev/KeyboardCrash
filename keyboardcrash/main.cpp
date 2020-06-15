b9e39bbfb694de98cbaafc705473243428bb2926
skiyedev <65785285+skiyedev@users.noreply.github.com>
2020-06-15 18:14:42 +0300

    Update main.cpp

--- keyboardcrash/main.cpp
+++ keyboardcrash/main.cpp
@@ -5,6 +5,7 @@
 #include "Key3.h"
 #include "Key4.h"
 #include "Score.h"
+#include "SpawnArrow.h"
 #include <QGraphicsView>
 #include <QTimer>
 
@@ -21,33 +22,34 @@ int main(int argc, char *argv[])
     Key2 * key2 = new Key2();
     Key3 * key3 = new Key3();
     Key4 * key4 = new Key4();
+    SpawnArrow * spawn = new SpawnArrow();
 
     Score * score;
 
-
+    key1->setFlag(QGraphicsPixmapItem::ItemIsFocusable);
     key1->setPixmap(QPixmap(":/Textures/Left.png"));
     key1->setPos(0,750);
     scene->addItem(key1);
     key1->setFocus();
 
-    key2->setFlag(QGraphicsRectItem::ItemIsFocusable);
+    key2->setFlag(QGraphicsPixmapItem::ItemIsFocusable);
     key2->setPixmap(QPixmap(":/Textures/Down.png"));
     key2->setPos(100,750);
     scene->addItem(key2);
     key2->setFocus();
 
-    key3->setFlag(QGraphicsRectItem::ItemIsFocusable);
+    key3->setFlag(QGraphicsPixmapItem::ItemIsFocusable);
     key3->setPixmap(QPixmap(":/Textures/Up.png"));
     key3->setPos(200,750);
     scene->addItem(key3);
     key3->setFocus();
 
-    key4->setFlag(QGraphicsRectItem::ItemIsFocusable);
+    key4->setFlag(QGraphicsPixmapItem::ItemIsFocusable);
     key4->setPixmap(QPixmap(":/Textures/Right.png"));
     key4->setPos(300,750);
     scene->addItem(key4);
     key4->setFocus();
-key1->setFlag(QGraphicsRectItem::ItemIsFocusable);
+
     view->setHorizontalScrollBarPolicy(Qt::ScrollBarAlwaysOff);
     view->setVerticalScrollBarPolicy(Qt::ScrollBarAlwaysOff);
     view->show();
@@ -61,7 +63,7 @@ key1->setFlag(QGraphicsRectItem::ItemIsFocusable);
     // spawn notes
 
     QTimer * timer = new QTimer();
-    QObject::connect(timer,SIGNAL(timeout()),key1,SLOT(spawn()));
+    QObject::connect(timer,SIGNAL(timeout()),spawn,SLOT(spawnArrow()));
     timer->start(300);
 
     return a.exec();
