bb72a6914d9fec48c9b3e9f4eb6e95b1b26f2204
skiyedev <65785285+skiyedev@users.noreply.github.com>
2020-06-15 18:13:44 +0300

    Delete Key4.cpp

--- keyboardcrash/Key4.cpp
+++
@@ -1,9 +0,0 @@
-#include "Key4.h"
-#include <QKeyEvent>
-#include <QGraphicsScene>
-#include "Note4.h"
-#include <stdlib.h>
-void Key4::keyPressEvent(QKeyEvent *event)
-{
-    if (event->key() == Qt::Key_O){}
-}
